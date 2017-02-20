1f3575c9693e934e15a991ecc8c66aada62c5cea
betrakiss <angelin.nedelchev@outlook.com>
2017-02-20 23:11:51 +0200

    Add song duration.

---
+++ src/UniversalPlayer/UniversalPlayer/Converters/DurationConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UniversalPlayer.Converters
+{
+    public class DurationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is TimeSpan)
+            {
+                TimeSpan span = (TimeSpan)value;
+                return new DateTime(span.Ticks).ToString("mm:ss");
+            }
+
+            return String.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
