fe802cba0843d23ca99127842095af4c39567ab1
betrakiss <angelin.nedelchev@outlook.com>
2017-02-21 22:15:53 +0200

    Song details now displayed in SMTC.

--- src/UniversalPlayer/UniversalPlayer/MainPage.xaml.cs
+++ src/UniversalPlayer/UniversalPlayer/MainPage.xaml.cs
@@ -5,10 +5,14 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using UniversalPlayer.Managers;
 using UniversalPlayer.Pages;
 using UniversalPlayer.ViewModels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Core;
+using Windows.Media.Playback;
 using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -28,12 +32,27 @@ namespace UniversalPlayer
     public sealed partial class MainPage : Page
     {
         private static bool loaded;
+        private MediaPlayer player = new MediaPlayer();
         public MainViewModel MainViewModel { get; set; } = new MainViewModel();
+
         public MainPage()
         {
             this.InitializeComponent();
 
+
+            var _smtc = SystemMediaTransportControls.GetForCurrentView();
+            _smtc.IsEnabled = true;
+            _smtc.IsPlayEnabled = true;
+            _smtc.IsPauseEnabled = true;
+            _smtc.IsStopEnabled = true;
+            _smtc.IsNextEnabled = true;
+            _smtc.IsPreviousEnabled = true;
+            _smtc.PlaybackStatus = MediaPlaybackStatus.Closed;
+            _smtc.AutoRepeatMode = MediaPlaybackAutoRepeatMode.Track;
+            
+
             DataContext = MainViewModel;
+            
             Loaded += MainPage_Loaded;
         }
 
@@ -53,6 +72,17 @@ namespace UniversalPlayer
                 foreach (SongViewModel song in e.Parameter as List<SongViewModel>)
                     MainViewModel.Songs.Add(song);
             }
+        }
+
+        private async void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SongViewModel vm = e.AddedItems[0] as SongViewModel;
+            StorageFile song = vm.FileHandle;
+
+            MediaPlaybackItem item = await MediaManager.CreateMediaPlaybackItemAsync(song);
+            
+            player.Source = item;
+            player.Play();
         }
     }
 }
---
+++ src/UniversalPlayer/UniversalPlayer/Managers/MediaManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+
+namespace UniversalPlayer.Managers
+{
+    public static class MediaManager
+    {
+        public static async Task<MediaPlaybackItem> CreateMediaPlaybackItemAsync(StorageFile song)
+        {
+            MediaPlaybackItem item = new MediaPlaybackItem(MediaSource.CreateFromStorageFile(song));
+            MediaItemDisplayProperties properties = item.GetDisplayProperties();
+            MusicProperties musicProperties = await song.Properties.GetMusicPropertiesAsync();
+
+            properties.Type = Windows.Media.MediaPlaybackType.Music;
+            properties.MusicProperties.Artist = musicProperties.Artist;
+            properties.MusicProperties.Title = musicProperties.Title;
+
+            item.ApplyDisplayProperties(properties);
+            return item;
+        }
+    }
+
+}
